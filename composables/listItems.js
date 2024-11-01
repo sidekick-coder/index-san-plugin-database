b30bfdbd90fba3d3392e80a94fa7fcb40331cee7
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-11-01 12:29:23 -0300

    refactor: better error feedback on list items

--- composables/listItems.js
+++ composables/listItems.js
@@ -3,6 +3,9 @@ import { showCollection } from './showCollection.js'
 import { showDatabase } from './showDatabase.js'
 import { showProvider } from './showProvider.js'
 
+import snackbar from 'app:snackbar'
+import { tryCatch } from 'app:utils'
+
 export async function listItems(databaseId, collectionId) {
     const database = await showDatabase(databaseId)
     const collection = await showCollection(databaseId, collectionId)
@@ -11,12 +14,17 @@ export async function listItems(databaseId, collectionId) {
     const provider = await showProvider(database.provider)
 
     if (!provider) {
-        throw new Error('Provider not found')
+        snackbar.error('Provider not found')
+        return []
     }
 
-    return provider.list({
-        database,
-        collection,
-        properties,
-    })
+    const [items, error] = await tryCatch(() => provider.list({ database, collection, properties }))
+
+    if (error) {
+        snackbar.error('Failed to list items')
+        console.error('[database] Failed to list items', error)
+        return []
+    }
+
+    return items
 }
